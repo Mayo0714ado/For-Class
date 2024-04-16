cb1e425aa54dfa60abdf88fecd236ee80562433e
Mayo0714ado <151921809+Mayo0714ado@users.noreply.github.com>
2024-04-16 22:19:51 +0800

    Update junjun.java

--- junjun.java
+++ junjun.java
@@ -1,29 +1,29 @@
 public class BalagotArrayExercises{
-  public static void main (String [] args) {
-  //letterOfAlphabets();
-  //ageOfStudents();
-  //salaryOfEmployees();
-  //booleanValues();
-    gradeOfStudents();
-  }
-  public static void letterOfAlphabets() {  
-    char[] alphabet = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J'};
+	public static void main (String [] args) {
+		letterOfAlphabets();
+		ageOfStudents();
+		salaryOfEmployees();
+		booleanValues();
+		gradeOfStudents();
+	}
+	public static void letterOfAlphabets() {  
+		char[] alphabet = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J'};
   
-  }
-  public static void ageOfStudents() {
-    int[] studentAges = {16, 20, 17, 18, 17};
+	}
+	public static void ageOfStudents() {
+		int[] studentAges = {16, 20, 17, 18, 17};
  
-  }
-  public static void salaryOfEmployees() {
-    int[] employeeSalaries = new int[0];
+	}
+	public static void salaryOfEmployees() {
+		int[] employeeSalaries = new int[0];
 
-  }  
-  public static void booleanValues() {
-    boolean[] booleanValues = {true, false, true, false};
+	}  
+	public static void booleanValues() {
+		boolean[] booleanValues = {true, false, true, false};
 
-  }
-  public static void gradeOfStudents(){
-    char[] studentGrades = {'A', 'B', 'C', 'A', 'A', 'B', 'C'};
+	}
+	public static void gradeOfStudents(){
+		char[] studentGrades = {'A', 'B', 'C', 'A', 'A', 'B', 'C'};
  
-  }
+	}
 }
