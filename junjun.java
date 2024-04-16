3d1bdc3efa65f9782e58c3e162a5b97139c0d02d
Jmbalagot <167201859+Jmbalagot@users.noreply.github.com>
2024-04-16 21:16:35 +0800

    Update junjun.java

--- junjun.java
+++ junjun.java
@@ -1,6 +1,6 @@
 public class BalagotArrayExercises{
   public static void main (String [] args){
-    
+    letterOfAlphabets();
   }
   public static void letterOfAlphabets(){  
     char[] alphabet = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J'};
