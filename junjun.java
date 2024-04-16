a6745a9a867f8e651897d5aa8271c8153e4041ff
Jmbalagot <167201859+Jmbalagot@users.noreply.github.com>
2024-04-16 21:55:32 +0800

    Update junjun.java

--- junjun.java
+++ junjun.java
@@ -1,19 +1,23 @@
 public class BalagotArrayExercises{
-  public static void main (String [] args){
+  public static void main (String [] args) {
   //letterOfAlphabets();
   //ageOfStudents();
-    salaryOfEmployees();
+  //salaryOfEmployees();
     
   }
-  public static void letterOfAlphabets(){  
+  public static void letterOfAlphabets() {  
     char[] alphabet = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J'};
   
   }
-  public static void ageOfStudents(){
+  public static void ageOfStudents() {
     int[] studentAges = {16, 20, 17, 18, 17};
  
   }
-  public static void salaryOfEmployees(){
-    
+  public static void salaryOfEmployees() {
+    int[] employeeSalaries = new int[0];
   }  
+  public static void booleanValues() {
+    
+  }
+  
 }
